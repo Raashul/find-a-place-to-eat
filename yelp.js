88c3b91931db308f57201549df89f11a146ff521
Raashul <rashul1996@gmail.com>
2017-04-25 21:22:31 -0400

    Final push

--- yelp.js
+++ yelp.js
@@ -9,34 +9,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 //Request API access: http://www.yelp.com/developers/getting_started/api_access
-		var Yelp = require('yelp');
+var Yelp = require('yelp');
 
-		var yelp = new Yelp({
-		  consumer_key: 'WfydRv01aFXZWK6TaAJOnQ',
-		  consumer_secret: 'oh9wyIny60Vc_edK4MBjCDHL0Bs',
-		  token: 'n7GVnnmsVoXxjoRdP9fxeDpmJ17DqzVk',
-		  token_secret: 'Y_ZibdstQn15p5eTjequFnFEo_A',
-		});
+//use your yelp credentials
+var yelp = new Yelp({
+  consumer_key: '',
+  consumer_secret: '',
+  token: '',
+  token_secret: '',
+});
 
 
 //Posting data
 app.post('/getyelp', function(req, res){
 
-	console.log('post request here');
-
 	var searchRestaurant		= req.body.restaurant;
 	var yelpLocation 				= req.body.location;
-
-	console.log(searchRestaurant);
-	console.log(yelpLocation);
-
-
 
 	// See http://www.yelp.com/developers/documentation/v2/search_api
 	yelp.search({ location: yelpLocation, term: searchRestaurant})
 	.then(function (data) {
-
-		console.log(data[0]);
 
 		console.log('Sending data to controller');
 		res.json(data); // I will return all the data recieved from YELP API. I will filter the data in controller.js
@@ -48,11 +40,6 @@ app.post('/getyelp', function(req, res){
 	});
 
 })
-
-// console.log('Port running in 3000');
-// app.listen(3000);
-
-
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
